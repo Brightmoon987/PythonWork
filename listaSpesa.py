e9d4e477c12230cd0c91a42fd2cdb4dfcc32c92a
alessio.mancuso <mancuso.alessio@iisgalvanimi.edu.it>
2024-11-20 10:57:27 +0100

    aggiunta

--- listaSpesa.py
+++ listaSpesa.py
@@ -1,19 +1,55 @@
-list = []
+lista = []
+
+
+
 
 
 def aggiungi_elementi_lista():
     
     x = input("aggiungi elemento alla lista: ")
     
-    list.append(x)
+    lista.append(x)
     
     
+
+
+
+
 def visualizza_lista(): 
     
-    for i in range(len(list)):
-        print(f"{i + 1}. {list[i]}")
+    for i in range(len(lista)):
+        print(f"{i + 1}. {lista[i]}")
         
-     
+
+
+
+def elimina_elemento_lista():
+    
+    domanda_Eliminazione = input("come vuoi eliminare l'elemento della lista?(scegli 1 se vuoi scrivere l'elemento,2 se vuoi eliminarlo tramite indice) ")
+    
+    
+    if domanda_Eliminazione == 1:
+        
+        precisa_eliminazione = input("dimmi l'elemento che vuoi eliminare: ")
+    
+        lista.remove(precisa_eliminazione)
+    
+    
+    elif domanda_Eliminazione == 2:
+        
+        eliminazione_indice = input("dimmi la posizione dell'elemento che vuoi eliminare: ")
+        
+        lista.pop(eliminazione_indice -1) 
+        
+        
+    
+    
+    
+    
+
+
+
+        
     
 
 
