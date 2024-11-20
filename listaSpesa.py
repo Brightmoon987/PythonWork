390451c0b67131021c9f64106049996e223f4e56
alessio.mancuso <mancuso.alessio@iisgalvanimi.edu.it>
2024-11-20 10:40:40 +0100

    file aggiunto

--- listaSpesa.py
+++ listaSpesa.py
@@ -8,7 +8,12 @@ def aggiungi_elementi_lista():
     list.append(x)
     
     
+def visualizza_lista(): 
     
+    for i in range(len(list)):
+        print(f"{i + 1}. {list[i]}")
+        
+     
     
 
 
